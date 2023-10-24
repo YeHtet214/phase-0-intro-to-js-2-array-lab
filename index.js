1aff7ad4c03f6a46f4867af5f619dd8ac2a7e2ee
Ye Htet <yhtet1934@gmail.com>
2023-10-24 22:41:50 +0630

    complete array-lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = [];
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(cat) {
+    return cats.pop(cat);
+}
+
+function destructivelyRemoveFirstCat(cat) {
+    return cats.shift(cat);
+}
+
+function appendCat(cat) {
+    return [...cats, cat];
+}
+
+function prependCat(cat) {
+    return [cat, ...cats];
+}
+
+function removeLastCat(cat) {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(cat) {
+    return cats.slice(1);
+}
+
